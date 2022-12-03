87aa085f9e8aab8f257f218594715591d6584632
jgarr27 <117048949+jgarr27@users.noreply.github.com>
2022-12-03 10:17:49 -0500

    push try 1

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace gcdeliverable1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool trigger = false;
+            while (trigger == false) {
+
+                Console.WriteLine("How many people are we makig pb&j sandwiches for? (integer people) ");
+                int people = int.Parse(Console.ReadLine());
+                decimal slices = people * 2;
+                decimal tablespoons = people * 2;
+                decimal teaspoons = people * 4;
+                decimal loaves = slices / 28;
+                decimal jarpb = tablespoons / 32;
+                decimal jarje = teaspoons / 48;
+
+                Console.WriteLine("you need:");
+                Console.WriteLine("slices of bread " + slices);
+                Console.WriteLine("tablespoons of peanut butter " + tablespoons);
+                Console.WriteLine("teaspoons of jelly " + teaspoons);
+
+                Console.WriteLine("which is...");
+
+                Console.WriteLine("loaves of bread " + Math.Ceiling(loaves));
+                Console.WriteLine("jars of peanut butter " + Math.Ceiling(jarpb));
+                Console.WriteLine("jars of jelly" + Math.Ceiling(jarje));
+
+                Console.Write("Would you like to restart? Enter yes or y to continue, or enter any other key to exit. : ");
+                string cont = (Console.ReadLine());
+                trigger = false;
+                if (cont != "yes")
+                    if (cont != "y")
+                    {
+                        if (trigger == false)
+
+                            Console.WriteLine("Goodbye");
+                        break;
+
+                    }
+
+
+            }
+        }
+    } }
